d38d3eb162c6cf0522a82cda0082086f670800c9
jstockwin <jstockwin@gmail.com>
2017-01-09 15:37:14 +0000

    Remove debug console.logs

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -9,16 +9,6 @@ module.exports = (function (settings) {
 	settings.test_settings.default.exclude = process.env.KNE_EXCLUDE_TEST_PATHS.split(',');
 	settings.globals_path = path.resolve(__dirname, 'globals.js');
 	settings.test_settings['saucelabs-travis'].desiredCapabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
-
-	console.log('****DEBUG KNE CONFIG*****');
-	console.log('src_folders', settings.src_folders);
-	console.log('po path', settings.page_objects_path);
-	console.log('exclude', settings.test_settings.default.exclude);
-	console.log('globals', settings.globals_path);
-	console.log('tunnelIdenfifier', settings.test_settings['saucelabs-travis'].desiredCapabilities['tunnel-identifier']);
-	console.log('start_process', process.env.KNE_SELENIUM_START_PROCESS); // set in .json
-	console.log('server start', process.env.KNE_SELENIUM_SERVER); // set in .json
-
 	return settings;
 
 })(require('./nightwatch.json'));
