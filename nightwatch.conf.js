d8e3991cea616ea15b528ee9f12816d3396a208a
Carlos Colon <webteckie@gmail.com>
2017-01-16 17:44:16 -0500

    General refactoring and chrome driver support. (#27)
    * General refactoring.
    
    * Fix lint issues.
    
    * Address code review comments.
    
    * Correct the chrome driver path.
    
    * Correct chromedriver path [again].  Bump version.

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,18 +1,39 @@
 var path = require('path');
 
 module.exports = (function (settings) {
+	var isWindows = /^win/.test(process.platform);
+	// TODO:  verify the chromedriver path in linux/mac
+	var chromeDriver = isWindows ? path.join(__dirname, 'node_modules/chromedriver/lib/chromedriver', 'chromedriver.exe') : path.join(__dirname, 'node_modules/chromedriver/lib/chromedriver', 'chromedriver');
+	var geckoDriver = isWindows ? path.join(__dirname, 'node_modules/geckodriver/geckodriver.exe') : path.join(__dirname, 'node_modules/.bin/geckodriver');
+
 	// The following environment variables are set to comma separated strings in index.js
 	// Here we will convert them to an array, as required by nightwatch.
-
 	settings.src_folders = process.env.KNE_TEST_PATHS.split(',');
 	settings.page_objects_path = process.env.KNE_PAGE_OBJECT_PATHS.split(',');
 	settings.test_settings.default.exclude = process.env.KNE_EXCLUDE_TEST_PATHS.split(',');
+
 	settings.globals_path = path.resolve(__dirname, 'globals.js');
-	settings.test_settings['saucelabs-travis'].desiredCapabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
-	settings.test_settings.default.cli_args['webdriver.gecko.driver'] = path.resolve(__dirname, 'node_modules/.bin/geckodriver');
-	settings.test_settings['saucelabs-travis'].cli_args['webdriver.gecko.driver'] = path.resolve(__dirname, 'node_modules/.bin/geckodriver');
-	settings.test_settings['saucelabs-local'].cli_args['webdriver.gecko.driver'] = path.resolve(__dirname, 'node_modules/.bin/geckodriver');
-	settings.test_settings['firefox-windows'].cli_args['webdriver.gecko.driver'] = path.resolve(__dirname, 'node_modules/geckodriver/geckodriver.exe');
+	settings.selenium.cli_args['webdriver.chrome.driver'] = chromeDriver;
+	settings.selenium.cli_args['webdriver.gecko.driver'] = geckoDriver;
+
+	if (process.env.TRAVIS_JOB_NUMBER) {
+		settings.test_settings['saucelabs-travis'].desiredCapabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
+	}
+
+	if (process.env.KNE_BROWSER_NAME) {
+		settings.test_settings[process.env.KNE_TEST_ENV].desiredCapabilities.browserName = process.env.KNE_BROWSER_NAME;
+	}
+
+	if (process.env.KNE_BROWSER_VERSION) {
+		settings.test_settings[process.env.KNE_TEST_ENV].desiredCapabilities.version = process.env.KNE_BROWSER_VERSION;
+	}
+
+	console.log('nightwatch settings:'
+		+ '\n\tenvironment: ' + process.env.KNE_TEST_ENV
+		+ '\n\tbrowser name: ' + settings.test_settings[process.env.KNE_TEST_ENV].desiredCapabilities.browserName
+		+ '\n\tbrowser version: ' + settings.test_settings[process.env.KNE_TEST_ENV].desiredCapabilities.version
+	);
+
 	return settings;
 
 })(require('./nightwatch.json'));
